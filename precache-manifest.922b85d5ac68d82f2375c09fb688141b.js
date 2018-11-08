ccc2599c38ede2f1631ac302ec21cf083e653a28
Jair de Souza Junior <detento112@gmail.com>
2018-11-07 23:48:07 -0200

    Updates

---
+++ precache-manifest.922b85d5ac68d82f2375c09fb688141b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "886af685ac820f267e4c2269a001b556",
+    "url": "/hangman-mqtt/static/media/man.886af685.png"
+  },
+  {
+    "revision": "696c4bd9432449cc3957",
+    "url": "/hangman-mqtt/static/js/runtime~main.696c4bd9.js"
+  },
+  {
+    "revision": "90050b3677b1082fb09d",
+    "url": "/hangman-mqtt/static/js/main.90050b36.chunk.js"
+  },
+  {
+    "revision": "dcb9a9614a62737fe0be",
+    "url": "/hangman-mqtt/static/js/1.dcb9a961.chunk.js"
+  },
+  {
+    "revision": "90050b3677b1082fb09d",
+    "url": "/hangman-mqtt/static/css/main.5c94e060.chunk.css"
+  },
+  {
+    "revision": "dcb9a9614a62737fe0be",
+    "url": "/hangman-mqtt/static/css/1.e722cb11.chunk.css"
+  },
+  {
+    "revision": "6a1c40dd2f5d7e981bc927d4cb4c51cc",
+    "url": "/hangman-mqtt/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efc6488239ff6307032c5dcdbf46be78.js"
+  "/hangman-mqtt/precache-manifest.922b85d5ac68d82f2375c09fb688141b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hangman-mqtt/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
